8be5551ae66fcf98705e57dd31ca75dba7af0663
Larrissa Yeo <yxtlar@gmail.com>
2024-04-01 01:43:50 +0800

    Retriveal of stuff from realtime DB

--- web_flask/main.py
+++ web_flask/main.py
@@ -1,79 +1,39 @@
 from flask import Flask, render_template, redirect, request, url_for
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
  
- 
+# Firebase
+from firebase import firebase
+from firebase_admin import credentials, firestore, initialize_app
+
+firebase =  firebase.FirebaseApplication('https://honeypwn-f7521-default-rtdb.firebaseio.com/', None)
+
 # Create a flask application
 app = Flask(__name__)
  
-# Tells flask-sqlalchemy what database to connect to
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-# Enter a secret key
-app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
-# Initialize flask-sqlalchemy extension
-db = SQLAlchemy()
- 
-# LoginManager is needed for our application 
-# to be able to log in and out users
-login_manager = LoginManager()
-login_manager.init_app(app)
+ # Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
 
-# Create user model
-class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(250), unique=True,
-                         nullable=False)
-    password = db.Column(db.String(250),
-                         nullable=False)
- 
- 
-# Initialize app with extension
-db.init_app(app)
-# Create database within app context
- 
-with app.app_context():
-    db.create_all()
+# @app.route('/register', methods=["GET", "POST"])
+# def register():
 
-# Creates a user loader callback that returns the user object given an id
-@login_manager.user_loader
-def loader_user(user_id):
-    return Users.query.get(user_id)
+#     return render_template("sign_up.html")
 
-@app.route('/register', methods=["GET", "POST"])
-def register():
-  # If the user made a POST request, create a new user
-    if request.method == "POST":
-        user = Users(username=request.form.get("username"),
-                     password=request.form.get("password"))
-        # Add the user to the database
-        db.session.add(user)
-        # Commit the changes made
-        db.session.commit()
-        # Once user account created, redirect them
-        # to login route (created later on)
-        return redirect(url_for("login"))
-    # Renders sign_up template if user made a GET request
-    return render_template("sign_up.html")
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        user = Users.query.filter_by(
-            username=request.form.get("username")).first()
-        if user.password == request.form.get("password"):
-            login_user(user)
-            return redirect(url_for("home"))
-    return render_template("login.html")
+#     return render_template("login.html")
 
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for("home"))
+# @app.route("/logout")
+# def logout():
+#     return redirect(url_for("home"))
 
 @app.route("/")
 def home():
     # Render home.html on "/" route
-    return render_template("home.html")
+    result = firebase.get('/users', None)
+    # return str(result)
+    return render_template("home.html", results = result)
 
 if __name__ == "__main__":
     app.run()
